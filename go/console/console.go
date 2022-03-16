9ad1bbdd3b98a4c15217f81a64dcfc972ef4b362
Ben Coomes <bcoomes96@gmail.com>
2022-03-16 07:54:14 -0400

    Parse transaction file contents

--- go/console/console.go
+++ go/console/console.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
 )
 
@@ -34,22 +36,82 @@ func check(e error) {
 }
 
 func ReadTransactionsFromFile(path string) []Transaction {
+
+	var contents = ReadAsString(path)
+	lines := strings.Split(contents, "\n")
+
+	var transactions []Transaction
+
+	for _, line := range lines {
+		transaction, err := ParseTransaction(line)
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			transactions = append(transactions, transaction)
+		}
+	}
+
+	return transactions
+}
+
+const expectedSegments = 3
+
+func ParseTransaction(raw string) (Transaction, error) {
+	segments := strings.Split(raw, ",")
+	actualSegments := len(segments)
+	if actualSegments != expectedSegments {
+		errtext := fmt.Sprintf("Expected %v items in raw string but found %v", expectedSegments, actualSegments)
+		return Transaction{}, errors.New(errtext)
+	}
+
+	date, err := ParseDate(segments[0])
+	if err != nil {
+		return Transaction{}, err
+	}
+
+	amount, err := ParseAmount(segments[1])
+	if err != nil {
+		return Transaction{}, err
+	}
+
+	details, err := ParseDetails(segments[2])
+	if err != nil {
+		return Transaction{}, err
+	}
+
+	return Transaction{
+		date:    date,
+		amount:  amount,
+		details: details,
+	}, nil
+}
+
+func ParseDate(dateString string) (string, error) {
+	return strings.TrimSpace(dateString), nil
+}
+
+func ParseAmount(amountString string) (float32, error) {
+	amountString = strings.TrimSpace(amountString)
+
+	amount, err := strconv.ParseFloat(amountString, 32)
+	if err != nil {
+		errtext := fmt.Sprintf("Failed to parse amount into a number. String value: %v", amountString)
+		return 0, errors.New(errtext)
+	}
+
+	return float32(amount), nil
+}
+
+func ParseDetails(detailsString string) (string, error) {
+	return strings.TrimSpace(detailsString), nil
+}
+
+func ReadAsString(path string) string {
 	absPath, err := filepath.Abs(path)
 	check(err)
 
 	contents, err := os.ReadFile(absPath)
 	check(err)
 
-	lines := strings.Split(string(contents), "\n")
-	transactions := make([]Transaction, len(lines))
-
-	for i, line := range lines {
-		transactions[i] = Transaction{
-			date:    "2021-12-01",
-			amount:  10.50,
-			details: line,
-		}
-	}
-
-	return transactions
+	return string(contents)
 }
